2a72e72596e1dfc5bd2a2e5fdbaaa38674b2815b
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-04-11 11:09:45 +0200

    Création routes vers page fichelogement et erreur dans router.jsx / Test OK

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Accueil from '../pages/accueil'
 import Apropos from '../pages/apropos'
+import Fichelogement from '../pages/fichelogement'
+import Erreur from '../pages/erreur'
 
 
 function Router() {
@@ -10,7 +12,9 @@ function Router() {
           
                 <Routes>
                     <Route path="/" element={<Accueil />} />
-                    <Route path="/apropos" element={<Apropos />} />                    
+                    <Route path="/apropos" element={<Apropos />} />
+                    <Route path="/fichelogement" element={<Fichelogement />} />
+                    <Route path="/erreur" element={<Erreur />} />                   
                 </Routes>
           
         </BrowserRouter>
