4eb178c169ee868e11893199dace023fd2db3585
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-04-22 08:25:06 +0200

    date copyright footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
         // afficher un pied de page avec un logo et du texte
         <footer className='footer'>
             <img src={logofooter} alt="Logo Kasa Footer"></img>
-            <div className='copyright'>&copy; 2023 Kasa. All rights reserved</div>
+            <div className='copyright'>&copy; 2024 Kasa. All rights reserved</div>
         </footer>
     )
 }
