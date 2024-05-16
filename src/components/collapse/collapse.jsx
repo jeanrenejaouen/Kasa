51c05171c7b2dc8b5b8c64d359acbecf6944aee5
jeanrenejaouen <jean-rene.jaouen@wanadoo.fr>
2024-05-16 13:03:22 +0200

    mod collapse

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useRef, useEffect, useState } from "react"
+import { useRef, useState } from "react"
 import arrowBack from '../../assets/icons/arrow_back.svg'
 import './_collapse.scss'
 
@@ -12,18 +12,9 @@ function Collapse(props) { /* arguments obtenus des composants parents */
     setIsExpanded((isExpanded) => !isExpanded);
     }
 
-/*     créer une "ref" connectée au paragraphe entourant "props.content" */
+/* créer une "ref" connectée au paragraphe entourant "props.content" */
 /* useRef permet de conserver la référence d'un élément du DOM pour en tirer quelques informations */
     const ref = useRef(null);
-
-    /* configurer la variable d'état "hauteur" pour stocker la hauteur du contenu une fois développé */
-    const [height, setHeight] = useState(0);
-
-    /* mettre à jour la variable "height" avec le "offsetHeight" de l'élément */
-    /* useEffect indique ce que le composant doit faire après le rendu. */
-        useEffect(() => {
-            setHeight (ref.current.offsetHeight)
-    }, []);
 
     return (
     /* événement onClick sur la flèche "arrowBack" pour appeler la fonction "toggleIsExpanded" après chaque clic */
@@ -36,9 +27,9 @@ function Collapse(props) { /* arguments obtenus des composants parents */
             <div onClick={toggleIsExpanded}>
                 {isExpanded ? <img src={arrowBack} alt="Chevron down"></img> : <img className= "rotate" src={arrowBack} alt="Chevron up"></img>}                
             </div>
-        </div>
+        </div>            
 
-        <div className={isExpanded ? 'collapseExpandOn' : 'collapseExpandOff'}>
+        <div onClick={toggleIsExpanded} className={isExpanded ? 'collapseExpandOn' : 'collapseExpandOff'}>
             <p ref={ref}>{props.content}</p>
         </div>
     </>
